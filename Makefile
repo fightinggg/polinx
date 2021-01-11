6ff8480006761080ae6543cbf1a9331044e3fc7d
hellowang <hellowang@example.com>
2021-01-11 21:22:41 +0800

    none

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 build :
-	nasm src/asm/polinx-boot.nasm -o target/boot.bin
-	dd if=target/boot.bin of=target/boot.img bs=512 count=2880
+	nasm -f macho32 -o target/boot.o src/asm/polinx-boot.asm 
+	nasm -f macho32  -o target/system.o src/asm/polinx.asm
+	gcc -m32 target/system.o target/boot.o
+	# dd if=target/boot.bin of=target/boot.img bs=512 count=1
+	# dd if=target/boot.bin of=target/boot.img bs=512 count=1
--- src/asm/polinx.asm
+++ src/asm/polinx.asm
@@ -1,85 +1,12 @@
+ORG 0x7c00+0x004200
 
-ORG 0x7c00 ; boot  0x7c00-0x7dff
+MOV AL, 0x13
+MOV AH, 0x00
+INT 0x19
 
-call main
+fin2: 
+  HLT
+  JMP fin2
 
-main: 
-    ; reg init
-    MOV AX,0 ; accumulator
-    MOV SS,AX ; stack segment
-    MOV DS,AX ; data segment
-    MOV ES,AX ; extra segment
-    MOV SP,0x7c00 ; stack pointer
-
-    MOV SI,startMessage ; source index
-    call printStr 
-
-    MOV SI,startMessage ; source index
-    call printStr 
-
-    MOV SI,startMessage ; source index
-    call printStr 
-    
-    call fin
-    ret
-
-
-printStr:
-    MOV AL,[SI]
-    ADD SI,1
-
-    CMP AL,0
-    JE printStrLeave
-
-    MOV AH,0x0e
-    MOV BX,15
-    INT 0x10
-    JMP printStr
-printStrLeave:
-    ret
-
-
-readDisk:
-    MOV AX,0x0820
-    MOV ES,AX
-    MOV CH,0 ; 柱面0
-    MOV DH,0 ; 磁头0
-    MOV CL,2 ; 扇区2
-
-    MOV SI,0
-    JMP readDiskLoop
-
-readDiskLoop:
-    MOV AH,0x02			; AH=0x02 : 读入磁盘
-    MOV AL,1			; 1个扇区
-    MOV BX,0
-    MOV DL,0x00			; A驱动器
-    INT 0x13			; 调用磁盘BIOS
-    JNC next			; 没出错则跳转到next
-    ADD SI,1			; 往SI加1
-    CMP SI,5			; 比较SI与5
-    JAE error			; SI >= 5 跳转到error
-    MOV AH,0x00
-    MOV DL,0x00			; A驱动器
-    INT 0x13			; 重置驱动器
-    JMP readDiskLoop
-
-next:
-   HLT
-   JMP next
-
-error:
-   HLT
-   JMP error
-
-fin:
-    HLT
-    JMP fin
-
-startMessage: 
-    DB "polinx start..."
-    DB 0x0a
-    DB 0
-
-times 510 - ($ - $$) db 0 ; fill with 0 untill arrive 512 bytes
-dw 0xaa55
+main:
+   JMP fin2
